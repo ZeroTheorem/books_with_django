a6406337a16513e8205d2b07b0acb8d2b81ddcc5
Alex Kovalev <zeratuliche@gmail.com>
2023-07-27 11:19:36 +0300

    ...

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,15 +1,15 @@
+from django.conf import settings
 from django.db import models
-from authnapp.models import my_User
 # Create your models here.
 
 
 class Users_books(models.Model):
 
-    owner = models.ForeignKey(my_User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='books')
     book_name = models.CharField(max_length=50)
     author_name = models.CharField(max_length=50)
     year_of_writing = models.PositiveIntegerField()
-    total_page = models.PositiveIntegerField()
-    current_page = models.PositiveIntegerField()
+    total_page = models.IntegerField()
+    current_page = models.IntegerField()
     description = models.TextField()
     book_image = models.ImageField(upload_to='images/')
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from mainapp.models import Users_books
 import os
+from django.db.models import F
+
 # Create your views here.
 
 
@@ -27,7 +29,7 @@ def main_page(request):
 
     else:
         if request.user.is_authenticated:
-            result = Users_books.objects.filter(owner=request.user)
+            result = Users_books.objects.filter(owner=request.user, current_page__gte=F('total_page'))
             books = len(result)
             pages = sum((x.total_page for x in result))
             content = {"result": result, "books": books, "pages": pages}
@@ -36,4 +38,25 @@ def main_page(request):
 
 
 def page_in_process(request):
-    return render(request, 'mainapp/process_page.html')
+    if request.method == "POST":
+        if request.POST.get('book_id'):
+            Users_books.objects.filter(pk=request.POST['book_id']).update(current_page=request.POST['update_current_page'])
+            return HttpResponseRedirect(reverse('process_books'))
+        else:
+            book = Users_books(owner=request.user,
+                        book_name=request.POST["book_name"],
+                        author_name=request.POST["author_name"],
+                        year_of_writing=request.POST["year_of_writing"],
+                        total_page=request.POST["total_page"],
+                        current_page=request.POST["current_page"],
+                        description=request.POST["description"],
+                        book_image=request.FILES['book_image'])
+            book.save()
+            return HttpResponseRedirect(reverse('process_books'))
+
+    if request.user.is_authenticated:
+            result = Users_books.objects.filter(owner=request.user, current_page__lt=F('total_page'))
+            books = len(result)
+            pages = sum((x.total_page for x in result))
+            content = {"result": result, "books": books, "pages": pages}
+            return render(request, 'mainapp/process_page.html', content)
