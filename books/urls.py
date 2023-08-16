8d596f1e34fe3608af764fc4bd84b3ccf9ac9dc6
q1110 <zeratuliche@gmail.com>
2023-08-16 12:39:49 +0300

    ...

--- books/urls.py
+++ books/urls.py
@@ -31,9 +31,9 @@ urlpatterns = [
     path("books/", include("mainapp.urls", namespace="mainapp")),
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
-    path("password-change/", PasswordChangeView.as_view(), name="password_cange"),
-    path("password-change/done", PasswordChangeDoneView.as_view(),
-         name="password_cange_done"),
+    path("password-change/", PasswordChangeView.as_view(), name="password_change"),
+    path("password-change/done/", PasswordChangeDoneView.as_view(),
+         name="password_change_done"),
 
 
 ]
